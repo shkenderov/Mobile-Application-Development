1275a1fcb23a172fa517eb1c6e39c8fdf6ece19e
uchitelq <stshk@abv.bg>
2020-03-10 09:51:34 +0000

    json added

--- week9/app/src/main/java/com/example/myapplication/MainActivity.java
+++ week9/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.myapplication;
-
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -25,6 +27,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         setContentView(R.layout.activity_main);
         Button btn=(Button)findViewById(R.id.btn1);
         btn.setOnClickListener(this);
+
     }
     public void onClick(View view) {
 
@@ -42,7 +45,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
                 EditText et1 = (EditText)findViewById(R.id.et);
 
                 String input = et1.getText().toString();
-                URL url = new URL("http://www.free-map.org.uk/course/ws/hits.php?artist=" + input);
+                URL url = new URL("http://www.free-map.org.uk/course/ws/hits.php?artist=" + input+"&format=json");
                 System.out.println("*************URL CALLED**********");
                 conn = (HttpURLConnection) url.openConnection();
                 InputStream in = conn.getInputStream();
@@ -50,7 +53,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
                     BufferedReader br = new BufferedReader(new InputStreamReader(in));
                     String result = "", line;
                     while ((line = br.readLine()) != null) {
-                        result += line + "\n";
+                        result += line;
                     }
                     return result;
                 } else {
@@ -68,8 +71,27 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         public void onPostExecute(String result)
         {
             TextView tv = (TextView) findViewById(R.id.txtview);
+            String text="";
 
-            tv.setText(result);
+            try {
+                JSONArray jsonArr = new JSONArray(result);
+                for(int i=0; i<jsonArr.length(); i++)
+                {
+                    JSONObject curObj = jsonArr.getJSONObject(i);
+                    String
+                            id = curObj.getString("ID"),
+                            title = curObj.getString("title"),
+                            artist = curObj.getString("artist"),
+                            genre = curObj.getString("genre");
+
+                    text +=" ID= "+ id + " Title: = " + title + " Artist= " + artist + " Genre: = " + genre + "\n";
+                }
+                tv.setText(text);
+            }
+            catch (JSONException e){
+                System.out.println ("An error occurred: " + e);
+        }
+        //tv.setText(result);
         }
     }
 }
